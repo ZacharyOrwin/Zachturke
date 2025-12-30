f66d1c670ba49ba46c7787f6ea3d907427c54e23
Jackie Tran <jackieooops@gmail.com>
2025-12-30 16:48:05 -0500

    Made some header functions for action buffering.

--- gamma/include/autonomous.hpp
+++ gamma/include/autonomous.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "api.h"
+#include <queue>
 
 
 namespace Autonomous {
@@ -27,11 +28,31 @@ namespace Autonomous {
                 void run_tick() override;
         };
 
+        struct Travel : public Action {
+                struct Config {
+                        float K;
+                        float B;
+                        float A;
+                        float dist;
+                        float epsilon;
+                        float timeout;
+                };
+
+                Config c;
+
+                Travel(Config c);
+                void run_tick() override;
+        };
+
         typedef std::pair<std::string, std::vector<std::unique_ptr<Action>>> Routine;
 
         extern std::shared_ptr<Routine> active_routine;
         // <Name (of routine), Routine>
         extern std::vector<std::shared_ptr<Routine>> routines;
+        extern std::queue<std::unique_ptr<Action>> actions_queue;
+        extern std::vector<std::unique_ptr<Action>> actions_running;
 
         void select_routine(int index);
+        void initialize_actions_queue(std::shared_ptr<Routine> routine);
+        void run_action(std::unique_ptr<Action>* action);
 }
